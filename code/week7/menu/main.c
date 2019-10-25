c6e17be3de54a748003d6a0c803b2c776d97111e
kevinyee628 <553407655@qq.com>
2019-10-25 02:41:21 -0700

    add main

---
+++ code/week7/menu/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+		printf("# # # # # # # # # # # # # # # # # #\n");
+		printf("\t#0.exit.                          \n");
+		printf("\t#1.create a new file.             \n");
+		printf("\t#2.write file						\n");
+		printf("\t#3.read file                      \n");
+		printf("\t#4.chmod                      	\n");
+		printf("\t#5.cat chmod                      \n");
+		printf("# # # # # # # # # # # # # # # # # #\n");
+		// menu();
+		return 0;
+}
